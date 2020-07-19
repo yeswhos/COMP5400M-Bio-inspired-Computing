6e119c7e6d4c4f278accff74e68666ded11e939d
yeswhos <yeswhos@outlook.com>
2020-07-19 18:15:00 +0100

    issue with line reading

--- CW1/beast/projects/shrew.cc
+++ CW1/beast/projects/shrew.cc
@@ -8,6 +8,8 @@
 #include "bacteria.h"
 #include<cstdlib>
 #include <unistd.h>
+#include "neuralanimat.h"
+#include "population.h"
 
 using namespace std;
 using namespace BEAST;
@@ -56,116 +58,52 @@ public:
 	// }
 };
 
-class Shrew : public Animat // Shrew is derived from Animat
-{
-public:
-	int controlMode;
-	Shrew()
-	{	
-		
-		This.Add("lineSensor", ProximitySensor<Shrew>(2 * PI, 300.0, -PI/20));
-		Sensors["lineSensor"] -> SetMatchingFunction(new MatchExact<circleCenter>);
-		This.Add("left", ProximitySensor<Shrew>(PI/5, 200.0, -PI/20));
-		This.Add("right", ProximitySensor<Shrew>(PI/5, 200.0, PI/20));
-		
-		Sensors["left"] -> SetMatchingFunction(new MatchExact<Shrew>);
-		Sensors["right"] -> SetMatchingFunction(new MatchExact<Shrew>);
-
-		//This.SetInitRandom(true);	// Start in random locations
-		This.InitRandom = false;
-		This.Radius = 28.0;			// Shrews are a little bigger than usual
-		SetMinSpeed(0.0);
-	}
-	
-	virtual ~Shrew(){
-		
-	}
-
-	void changeControlMode(int controlMode) {
-		This.controlMode = controlMode;
-	}
-
-	virtual void Control()
-	{
-		if(This.controlMode == 1) {
-			Mode1();
-		} else {
-			Mode2();
-		}
-
-	}
-	
-	void Mode1() {
-		// write mode 1
-		double right_a = This.Sensors["right"]->GetOutput();
-		double left_a = This.Sensors["left"]->GetOutput();
-		//double c = This.Sensors["line"] ->GetOutput();
-		//bool inRange = true;
-		double num = rand()/double(RAND_MAX);
-		//cout << num << endl;
-
-		// if ((c >= 22) && (c <= 29)){
-		// 	inRange = true;
-		// }else{
-		// 	inRange = false;
-		// }
-		// if (inRange){
-		// This.Controls["left"] = -left_a;
-		// This.Controls["right"] = -right_a;
-		This.Controls["left"] = 0;
-		This.Controls["right"] = 0;
-		//cout << b << endl;
-		// }else{
-		// 	This.Controls["left"] = 0.0;
-		// 	This.Controls["right"] = 0.5;
-		// 	//sleep(1);
-		// }
-
-		// This.Controls["left"] = This.Sensors["right"]->GetOutput();
-		// This.Controls["right"] = This.Sensors["left"]->GetOutput();
-		
-		//cout << c << endl;
-	}
-
-	void Mode2() {
-		// write mode 2
-		This.Controls["left"] = 0;
-		This.Controls["right"] = 0;
-	}
-};
-
-// class Shrew_opponent : public Animat // Shrew is derived from Animat
+// class Shrew : public Animat // Shrew is derived from Animat
 // {
 // public:
-	
-// 	Shrew_opponent()
+// 	int controlMode;
+// 	Shrew()
 // 	{	
 		
-// 		//need to add area sensor -PI/20
-// 		This.Add("lineSensor", ProximitySensor<Shrew_opponent>(2 * PI, 300.0, -PI/20));
+// 		This.Add("lineSensor", ProximitySensor<Shrew>(2 * PI, 300.0, -PI/20));
 // 		Sensors["lineSensor"] -> SetMatchingFunction(new MatchExact<circleCenter>);
-// 		This.Add("left", ProximitySensor<Shrew_opponent>(PI/5, 200.0, -(21 * PI)/20));
-// 		This.Add("right", ProximitySensor<Shrew_opponent>(PI/5, 200.0, (21 * PI)/20));
+// 		This.Add("left", ProximitySensor<Shrew>(PI/5, 200.0, -PI/20));
+// 		This.Add("right", ProximitySensor<Shrew>(PI/5, 200.0, PI/20));
+		
 // 		Sensors["left"] -> SetMatchingFunction(new MatchExact<Shrew>);
 // 		Sensors["right"] -> SetMatchingFunction(new MatchExact<Shrew>);
+
 // 		//This.SetInitRandom(true);	// Start in random locations
 // 		This.InitRandom = false;
-// 		SetStartLocation(Vector2D(500, 300));
-		
 // 		This.Radius = 28.0;			// Shrews are a little bigger than usual
 // 		SetMinSpeed(0.0);
 // 	}
 	
-// 	virtual ~Shrew_opponent(){
+// 	virtual ~Shrew(){
 		
+// 	}
+
+// 	void changeControlMode(int controlMode) {
+// 		This.controlMode = controlMode;
 // 	}
 
 // 	virtual void Control()
 // 	{
-// 		double a = This.Sensors["right"]->GetOutput();
+// 		if(This.controlMode == 1) {
+// 			Mode1();
+// 		} else {
+// 			Mode2();
+// 		}
+
+// 	}
+	
+// 	void Mode1() {
+// 		// write mode 1
+// 		double right_a = This.Sensors["right"]->GetOutput();
+// 		double left_a = This.Sensors["left"]->GetOutput();
+// 		//double line = This.Sensors["line"] ->GetOutput();
 // 		//bool inRange = true;
-// 		double b = This.Sensors["left"]->GetOutput();
-// 		double num = rand()/double(RAND_MAX);
+// 		//double num = rand()/double(RAND_MAX);
 // 		//cout << num << endl;
 
 // 		// if ((c >= 22) && (c <= 29)){
@@ -174,10 +112,11 @@ public:
 // 		// 	inRange = false;
 // 		// }
 // 		// if (inRange){
-// 		This.Controls["left"] = -a;
-// 		This.Controls["right"] = -b;
-// 		cout << a << endl;
-// 		cout << b << endl;
+// 		// This.Controls["left"] = -left_a;
+// 		// This.Controls["right"] = -right_a;
+// 		This.Controls["left"] = -right_a;
+// 		This.Controls["right"] = -left_a;
+// 		//cout << b << endl;
 // 		// }else{
 // 		// 	This.Controls["left"] = 0.0;
 // 		// 	This.Controls["right"] = 0.5;
@@ -188,35 +127,109 @@ public:
 // 		// This.Controls["right"] = This.Sensors["left"]->GetOutput();
 		
 // 		//cout << c << endl;
-		
+// 	}
 
+// 	void Mode2() {
+// 		// write mode 2
+// 		//double line = This.Sensors["line"] ->GetOutput();
+// 		double num = rand()/double(RAND_MAX);
+// 		double num_plus = rand()/double(RAND_MAX);
+// 		This.Controls["left"] = num;
+// 		This.Controls["right"] = num_plus;
+		
 // 	}
 // };
 
+class EvoMouses : public EvoFFNAnimat
+{
+public:
+	//double lines;
+	EvoMouses()
+	{
+		This.Add("lineSensor", ProximitySensor<EvoMouses>(2 * PI, 500.0f, -PI));
+		Sensors["lineSensor"] -> SetMatchingFunction(new MatchExact<circleCenter>);
+		This.Add("left", ProximitySensor<EvoMouses>(PI/5, 200.0, -PI/20));
+		This.Add("right", ProximitySensor<EvoMouses>(PI/5, 200.0, PI/20));
+		
+		Sensors["left"] -> SetMatchingFunction(new MatchExact<EvoMouses>);
+		Sensors["right"] -> SetMatchingFunction(new MatchExact<EvoMouses>);
+
+		//This.SetInitRandom(true);	// Start in random locations
+		This.InitRandom = false;
+		This.Radius = 28.0;
+		SetMinSpeed(0.0);
+		This.InitFFN(4);
+		// This.lines = This.Sensors["lineSensor"] ->GetOutput();
+		// cout << lines << endl;
+	}
+	// double lines = This.Sensors["lineSensor"] ->GetOutput();
+	// void setLines()
+	virtual void getLines(){
+		lines = This.Sensors["lineSensor"] ->GetOutput();
+
+		//EvoFFNAnimat::getLines();
+	}
+	virtual float GetFitness()const
+	{
+		//double lines = setLine();
+		cout << This.lines << endl;
+		// double right_a = This.Sensors["right"]->GetOutput();
+		if (lines < 200.0f){
+			return 0.0;
+		}
+		else{
+			return 1.0;
+		}
+	}
+
+	// Overloading the ToString method allows us to output a small amount of
+	// information which is visible in the status bar of the GUI when a
+	// mouse is clicked on.
+	virtual string ToString()const
+	{
+		
+		ostringstream out;
+		out << " Power used: " << This.PowerUsed;
+		return out.str();
+	}
+private:
+	double lines;
+
+};
+
 class ShrewSimulation : public Simulation
 {
-	Group<Shrew> grpShrew;
+	GeneticAlgorithm<EvoMouses> theGA;
+	Population<EvoMouses> theMice;
+	// Group<Shrew> grpShrew;
 	//Group<Shrew_opponent> grpShrew_opponent;
 	Group<Circle> theCircle;
 	Group<circleCenter> theCircleCenter;
 
 public:
 	ShrewSimulation():
+	theGA(0.7f, 0.05f),
+	theMice(2, theGA),
 	theCircle(1),
-	theCircleCenter(1),
+	theCircleCenter(1)
 	//grpShrew_opponent(1),
-	grpShrew(2)
+	// grpShrew(2)
 	{
-		grpShrew[0] -> SetStartLocation(Vector2D(500, 300));
-		grpShrew[1] -> SetStartLocation(Vector2D(300, 300));
-		grpShrew[1] -> changeControlMode(2);
-		grpShrew[0] -> changeControlMode(1);
-		grpShrew[0] -> Add("left", ProximitySensor<Shrew>(PI/5, 200.0, -(21 * PI)/20));
-		grpShrew[0] -> Add("right", ProximitySensor<Shrew>(PI/5, 200.0, (21 * PI)/20));
+		This.Add("Mice", This.theMice);
+		theMice[0] -> SetStartLocation(Vector2D(500, 300));
+		theMice[1] -> SetStartLocation(Vector2D(300, 300));
+		// grpShrew[1] -> changeControlMode(2);
+		// grpShrew[0] -> changeControlMode(1);
+		theMice[0] -> Add("left", ProximitySensor<EvoMouses>(PI/5, 200.0, -(21 * PI)/20));
+		theMice[0] -> Add("right", ProximitySensor<EvoMouses>(PI/5, 200.0, (21 * PI)/20));
 		This.Add("Circle", This.theCircle);
 		This.Add("circleCenter", This.theCircleCenter);
-		This.Add("Shrews", This.grpShrew);
+		
+		This.theGA.SetSelection(GA_TOURNAMENT);
+		This.theGA.SetParameter(GA_TOURNAMENT_SIZE, 2);
+		//This.Add("Shrews", This.grpShrew);
 		//This.Add("Shrews_opponent", This.grpShrew_opponent);
+		This.SetTimeSteps(200);
 	}
 };
 
