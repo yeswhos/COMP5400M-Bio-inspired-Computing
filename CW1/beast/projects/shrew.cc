ed397f2c89b21af816cec958ebcc11cc5769cf7a
yeswhos <yeswhos@outlook.com>
2020-07-23 15:55:21 +0100

    add two class and completed counter reading

--- CW1/beast/projects/shrew.cc
+++ CW1/beast/projects/shrew.cc
@@ -14,6 +14,8 @@
 using namespace std;
 using namespace BEAST;
 
+int counterA = 0;
+int counterB = 0;
 // For more information on this file, take a look at Tutorial 1 in the
 // BEAST documentation.
 class Circle : public WorldObject
@@ -52,22 +54,22 @@ public:
 	virtual ~circleCenter(){}
 };
 
+class EvoMousesOpponent;
 class EvoMouses : public EvoFFNAnimat
 {
 public:
 	//double lines;
 	bool inRange = true;
 	int counter = 0;
-	int count = 0;
 	
 	//EvoMouses *enermy;
 	EvoMouses():lines(0)
 	{
 		This.Add("lineSensor", ProximitySensor<circleCenter>(2 * PI, 200.0f, -PI));
 		//Sensors["lineSensor"] -> SetMatchingFunction(new MatchExact<circleCenter>);
-		This.Add("left", ProximitySensor<EvoMouses>(PI/5, 200.0, -PI/20));
-		This.Add("right", ProximitySensor<EvoMouses>(PI/5, 200.0, PI/20));
-
+		This.Add("left", ProximitySensor<EvoMousesOpponent>(PI/5, 200.0, -PI/20));
+		This.Add("right", ProximitySensor<EvoMousesOpponent>(PI/5, 200.0, PI/20));
+		This.SetStartOrientation(PI);
 		//This.SetInitRandom(true);	// Start in random locations
 		This.InitRandom = false;
 		This.SetStartLocation(Vector2D(500, 300));
@@ -95,37 +97,106 @@ public:
 
 	void OnCollision(WorldObject* obj){
 		if (inRange == false){
-			counter++;
+			counterA++;
 			This.SetLocation(Vector2D(500, 300));
 			inRange = true;
 		}
-		if(count == 2){
-			ofstream f("/home/beast/COMP5400M-Bio-inspired-Computing/CW1/beast/MSc/log.txt", ios::trunc);
-			count = 0;
-		}
-		ofstream out("/home/beast/COMP5400M-Bio-inspired-Computing/CW1/beast/MSc/log.txt", ios::app);
-		out << counter << endl;
-		count++;
-
-		out.close();
 		//setEnermy(enermy);
 		FFNAnimat::OnCollision(obj);
 	}
 
 	virtual float GetFitness()const
 	{	
-		// count++;
-
-		fstream f;
-		f.open("/home/beast/COMP5400M-Bio-inspired-Computing/CW1/beast/MSc/log.txt", ios::in);
-		char line[128];
-		f.getline(line, 128);
-		cout << line << endl;
-		// if(count == 2){
-		// 	ofstream f("/home/beast/COMP5400M-Bio-inspired-Computing/CW1/beast/MSc/log.txt", ios::trunc);
-		// 	count = 0;
+		cout << counterB << endl;
+		if (inRange){
+			return 1.0;
+		}
+		else{
+			return 0.0;
+		}
+		// int score;
+		// if (counter == 0){
+		// 	score = 1;
+		// 	return score;
+		// }else{
+		// 	float score = 1 / counter;
+		// 	cout << score << endl;
+		// 	return score;
 		// }
-		//cout << counter << endl;
+
+		// int score = enermy -> counter;
+		// float fitness = score - This.counter;
+		// return fitness;
+	}
+
+	// Overloading the ToString method allows us to output a small amount of
+	// information which is visible in the status bar of the GUI when a
+	// mouse is clicked on.
+	virtual string ToString()const
+	{
+		ostringstream out;
+		out << " Power used: " << This.PowerUsed;
+		return out.str();
+	}
+private:
+	double lines;
+
+};
+
+class EvoMousesOpponent : public EvoFFNAnimat
+{
+public:
+	//double lines;
+	bool inRange = true;
+	int counter = 0;
+	
+	//EvoMouses *enermy;
+	EvoMousesOpponent():lines(0)
+	{
+		This.Add("lineSensor", ProximitySensor<circleCenter>(2 * PI, 200.0f, -PI));
+		//Sensors["lineSensor"] -> SetMatchingFunction(new MatchExact<circleCenter>);
+		This.Add("left", ProximitySensor<EvoMouses>(PI/5, 200.0, -PI/20));
+		This.Add("right", ProximitySensor<EvoMouses>(PI/5, 200.0, PI/20));
+
+		//This.SetInitRandom(true);	// Start in random locations
+		This.InitRandom = false;
+		This.SetStartLocation(Vector2D(300, 300));
+		
+		This.Radius = 28.0;
+		SetMinSpeed(0.0);
+		This.InitFFN(5);
+
+	}
+
+	// void setEnermy(EvoMouses *enermy) {
+	// 	This.enermy = enermy;
+	// }
+
+	virtual void Control(){
+		This.lines = This.Sensors["lineSensor"] ->GetOutput();
+		if(This.lines < 0.015 && This.lines != 0){
+			inRange = false;
+		}
+		//cout << This.lines  << endl;
+		// This.Controls["left"] = 0;
+		// This.Controls["right"] = 0;
+		
+		EvoFFNAnimat::Control();
+	}
+
+	void OnCollision(WorldObject* obj){
+		if (inRange == false){
+			counterB++;
+			This.SetLocation(Vector2D(300, 300));
+			inRange = true;
+		}
+		//setEnermy(enermy);
+		FFNAnimat::OnCollision(obj);
+	}
+
+	virtual float GetFitness()const
+	{	
+		cout << counterA << endl;
 		if (inRange){
 			return 1.0;
 		}
@@ -163,37 +234,40 @@ private:
 
 class ShrewSimulation : public Simulation
 {
-	GeneticAlgorithm<EvoMouses> theGA;
-	Population<EvoMouses> theMice;
+	GeneticAlgorithm<EvoMouses> gaEvo;
+	GeneticAlgorithm<EvoMousesOpponent> gaEvoEnemy;
+	Population<EvoMouses> popEvo;
+	Population<EvoMousesOpponent> popEvoEnemy;
 	// Group<EvoMouses> grpShrew;
 	Group<Circle> theCircle;
 	Group<circleCenter> theCircleCenter;
 
 public:
 	ShrewSimulation():
-	theGA(0.7f, 0.05f),
-	theMice(2, theGA),
+	gaEvo(0.7f, 0.05f),
+	gaEvoEnemy(0.7f, 0.05f),
+	popEvo(5, gaEvo),
+	popEvoEnemy(5, gaEvoEnemy),
 	theCircle(1),
 	theCircleCenter(1)
 	{
-		This.Add("EvoMouses", This.theMice);
-		//theMice[0] -> SetLocation(500, 300);
-		//theMice[1] -> SetStartOrientation(PI);
-		// grpShrew[1] -> changeControlMode(2);
-		// grpShrew[0] -> changeControlMode(1);
-		// theMice[0] -> Add("left", ProximitySensor<EvoMouses>(PI/5, 200.0, -(21 * PI)/20));
-		// theMice[0] -> Add("right", ProximitySensor<EvoMouses>(PI/5, 200.0, (21 * PI)/20));
-		// tes = theMice[1] -> inRange;
-		// cout << tes << endl;
-		//theMice.setEnermy()
+		This.gaEvo.SetSelection(GA_TOURNAMENT);
+		This.gaEvo.SetParameter(GA_TOURNAMENT_SIZE, 1);
+		This.gaEvoEnemy.SetSelection(GA_TOURNAMENT);
+		This.gaEvoEnemy.SetParameter(GA_TOURNAMENT_SIZE, 1);
+		popEvo.SetTeamSize(1);
+		popEvoEnemy.SetTeamSize(1);
+		SetAssessments(5);
+		This.Add("EvoMouses", This.popEvo);
+		This.Add("EvoMousesEnemy", This.popEvoEnemy);
+		
 		This.Add("Circle", This.theCircle);
 		This.Add("circleCenter", This.theCircleCenter);
 		
-		This.theGA.SetSelection(GA_TOURNAMENT);
-		This.theGA.SetParameter(GA_TOURNAMENT_SIZE, 2);
+
 		//This.Add("Shrews", This.grpShrew);
 		//This.Add("Shrews_opponent", This.grpShrew_opponent);
-		This.SetTimeSteps(200);
+		//This.SetTimeSteps(200);
 	}
 };
 
